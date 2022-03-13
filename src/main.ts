4aed581231b537390a6e09a739d5c939f6844541
lodashreload <icarusjapan@gmail.com>
2022-03-13 12:36:12 -0400

    I'm changing everything ... lost?

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,49 @@
 import './style.css'
-console.clear()
 
-const playerOneRock = document.querySelector('.player1 i.rock')
-const playerOnePaper = document.querySelector('.player1 i.paper')
-const playerOneScissors = document.querySelector('.player1 i.scissors')
+const playerOneChoice = document.getElementById('player1')
+const playerTwoChoice = document.getElementById('player2')
+// console.clear()
 
-const playerTwoRock = document.querySelector('.player2 i.rock')
-const playerTwoPaper = document.querySelector('.player2 i.paper')
-const playerTwoScissors = document.querySelector('.player2 i.scissors')
+// const playerOneRock = document.querySelector('.player1 i.rock')
+// const playerOnePaper = document.querySelector('.player1 i.paper')
+// const playerOneScissors = document.querySelector('.player1 i.scissors')
 
-function handleClickOnPlayerOneRock() {
-  console.log('Rock')
-}
-playerOneRock?.addEventListener('click', handleClickOnPlayerOneRock)
+// const playerTwoRock = document.querySelector('.player2 i.rock')
+// const playerTwoPaper = document.querySelector('.player2 i.paper')
+// const playerTwoScissors = document.querySelector('.player2 i.scissors')
 
-function handleClickOnPlayerTwoPaper() {
-  console.log('Paper')
-}
-playerTwoPaper?.addEventListener('click', handleClickOnPlayerTwoPaper)
+// let p1 = ''
+// let p2 = ''
 
-console.log(playerOneRock)
-console.log(playerOnePaper)
-console.log(playerOneScissors)
-console.log(playerTwoRock)
-console.log(playerTwoPaper)
-console.log(playerTwoScissors)
+// // test functions
+// function handleClickOnPlayerOneRock() {
+//   p1 = 'rock'
+
+//   console.log('p1choice')
+// }
+// playerOneRock?.addEventListener('click', handleClickOnPlayerOneRock)
+
+// function handleClickOnPlayerTwoPaper() {
+//   p2 = 'paper'
+//   console.log('p2choice')
+// }
+
+// playerTwoPaper?.addEventListener('click', handleClickOnPlayerTwoPaper)
+// detectWinner
+// function detectWinner(p1: string, p2: string): string {
+//   if (
+//     (p1 == 'rock' && p2 == 'scissors') ||
+//     (p1 == 'scissors' && p2 == 'paper') ||
+//     (p1 == 'paper' && p2 == 'rock')
+//   )
+//     return 'Player 1 won!'
+//   else if (p1 === p2) return 'Draw!'
+//   else return 'Player 2 won!'
+// }
+
+// console.log(playerOneRock)
+// console.log(playerOnePaper)
+// console.log(playerOneScissors)
+// console.log(playerTwoRock)
+// console.log(playerTwoPaper)
+// console.log(playerTwoScissors)
