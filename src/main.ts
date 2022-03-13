b92c6d5e1ff0b171b7a455fc062050beea7a12a1
lodashreload <icarusjapan@gmail.com>
2022-03-13 16:26:48 -0400

    game works but only with the computer: needs lots of refactoriing

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import './style.css'
 
-const computerChoiceDisplay = document.getElementById('cc')
+const computerChoiceDisplay = document.getElementById('cc')!
 const userChoiceDisplay = document.getElementById('p1')!
-const resultDisplay = document.getElementById('result')
-
+const resultDisplay = document.getElementById('result')!
 const possibleChoices = document.querySelectorAll('button')
 
-let userChoice
-let computerChoice
+let userChoice: any
+let computerChoice: any
+let result: any
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
   })
 )
 
@@ -30,6 +31,31 @@ function generateComputerChoice() {
     computerChoice = 'paper'
   }
   computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = 'its a draw'
+  }
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = 'you win'
+  }
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = 'you lose'
+  }
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = 'you win'
+  }
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = 'you lose'
+  }
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = 'you win'
+  }
+  if (computerChoice === 'scissor' && userChoice === 'paper') {
+    result = 'you lose'
+  }
+  resultDisplay.innerHTML = result
 }
 // console.clear()
 
