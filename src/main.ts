b23ea5c056462d52247fde61014cc5747d7eebf7
lodashreload <icarusjapan@gmail.com>
2022-03-13 16:15:16 -0400

    randomly generate computer choice

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,36 @@
 import './style.css'
 
-const playerOneChoice = document.getElementById('player1')
-const playerTwoChoice = document.getElementById('player2')
+const computerChoiceDisplay = document.getElementById('cc')
+const userChoiceDisplay = document.getElementById('p1')!
+const resultDisplay = document.getElementById('result')
+
+const possibleChoices = document.querySelectorAll('button')
+
+let userChoice
+let computerChoice
+
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+  })
+)
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+
+  if (randomNumber === 1) {
+    computerChoice = 'rock'
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'scissors'
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'paper'
+  }
+  computerChoiceDisplay.innerHTML = computerChoice
+}
 // console.clear()
 
 // const playerOneRock = document.querySelector('.player1 i.rock')
