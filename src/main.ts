2bb6f30fed9e3eae22f3f18d9c3f0f250a03f9b0
lodashreload <icarusjapan@gmail.com>
2022-03-12 21:43:17 -0500

    const for locating rock, paper, and scissor for both player1 and player2

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-import "./style.css";
+import './style.css'
+console.clear()
+
+const playerOneRock = document.querySelector('.player1 i.rock')
+const playerOnePaper = document.querySelector('.player1 i.paper')
+const playerOneScissors = document.querySelector('.player1 i.scissors')
+
+const playerTwoRock = document.querySelector('.player2 i.rock')
+const playerTwoPaper = document.querySelector('.player2 i.paper')
+const playerTwoScissors = document.querySelector('.player2 i.scissors')
+
+console.log(playerOneRock)
+console.log(playerOnePaper)
+console.log(playerOneScissors)
+console.log(playerTwoRock)
+console.log(playerTwoPaper)
+console.log(playerTwoScissors)
